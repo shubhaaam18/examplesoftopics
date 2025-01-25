4fda8d3d5b426878b6d8d15eb1ad5761d42efa8d
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2025-01-25 19:12:05 +0530

    Create varletconst.js

---
+++ varletconst.js
@@ -0,0 +1,11 @@
+if (true) {
+  var city = "Mumbai";
+  let state = "Maharashtra";
+  const zipCode = 401; 
+
+  console.log(city);
+  console.log(state);
+  console.log(zipCode);
+}
+
+console.log(city);
