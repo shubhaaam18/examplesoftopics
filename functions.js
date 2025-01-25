d0ecaf0130f9c418f9c3af4db5a7da9896f0b636
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2025-01-25 19:14:37 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,24 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("Shubham"));
+
+
+
+const add = function (a, b) {
+  return a + b;
+};
+
+console.log(add(3, 5));
+
+
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(4, 7));
+
+
+setTimeout(function () {
+  console.log("shubham");
+}, 2000);
