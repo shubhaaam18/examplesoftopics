347ea8fa3121b4a707b56eb6d73bc106ee372187
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2025-02-01 12:49:56 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -22,3 +22,31 @@ console.log(multiply(4, 7));
 setTimeout(function () {
   console.log("shubham");
 }, 2000);
+
+
+//first class functions
+function operate(a, b, operation) {
+    return operation(a, b);
+}
+
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+console.log(operate(5, 3, add)); 
+console.log(operate(5, 3, multiply)); 
+
+
+//higher order function 
+function operate(a, b, operation) {
+    return operation(a, b);
+}
+const add = (x, y) => x + y;
+console.log(operate(5, 3, add));
+
+
+//IIFE
+(function(name) {
+    console.log(`Hello, ${name}!`);
+})("Shubham");
+
+
+
