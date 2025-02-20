b6c122b275739a34e220a7c32b30f17a4401f51c
shubhaaam18 <103482230+shubhaaam18@users.noreply.github.com>
2025-02-20 11:54:05 +0530

    Create sync-program.js

---
+++ sync-program.js
@@ -0,0 +1,15 @@
+console.log("start");
+
+function first()
+{
+    console.log("First function executed");
+}
+
+function second()
+{
+    console.log("Second function executed");
+}
+
+first();
+second();
+console.log("end");
